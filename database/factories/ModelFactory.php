c06bf4487945f0914679d6b41a7e539ba8531f5b
andela-jkariuki <john.kariuki@andela.com>
2016-06-09 00:45:18 +0300

    add migrations and user factory

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -15,6 +15,7 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->safeEmail,
+        'age' => rand(1, 100),
         'password' => bcrypt(str_random(10)),
         'remember_token' => str_random(10),
     ];
